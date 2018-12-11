1d9a2d54ffbb5bc080c8aa1fa99782bfe4b659d0
raa0121 <raa0121@gmail.com>
2018-12-11 23:18:23 +0900

    kokoro2mc

--- src/main/kotlin/com/github/raa0121/spicat/Spicat.kt
+++ src/main/kotlin/com/github/raa0121/spicat/Spicat.kt
@@ -1,10 +1,12 @@
 package com.github.raa0121.spicat
 
 import com.github.raa0121.kokoroio.model.MessageEntity
+import com.ryanharter.ktor.moshi.moshi
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
 import io.ktor.http.ContentType
 import io.ktor.request.receive
-import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -12,39 +14,50 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.bukkit.plugin.java.JavaPlugin
+import io.swagger.client.infrastructure.Serializer
 import java.io.File
+import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 class Spicat : JavaPlugin() {
 
     private val webserver = embeddedServer(Netty, 8080) {
+        install(ContentNegotiation) {
+            moshi(Serializer.moshi)
+        }
         routing {
             get("/") {
                 call.respondText("hi", ContentType.Text.Plain)
             }
             post("/") {
-                val post = call.receive<MessageEntity>()
-                server.broadcastMessage(
-                        "[kokoro-io/%s] %s: %s".format(
-                                post.channel?.channel_name,
-                                post.profile?.screen_name,
-                                post.raw_content
-                        )
-                )
-                call.respond(mapOf("OK" to true))
+                try {
+                    val post = call.receive<MessageEntity>()
+                    logger.info(post.channel?.channel_name)
+                    server.broadcastMessage(
+                             "[kokoro-io/%s] %s: %s".format(
+                                     post.channel?.channel_name,
+                                     post.profile?.screen_name,
+                                     post.raw_content
+                             )
+                    )
+                } catch (e: Exception) {
+                    logger.warning(e.toString())
+                }
             }
         }
     }
 
     override fun onEnable() {
         // Plugin startup logic
+        //notifier?.postMessage("Server starting.")
         webserver.start(wait = false)
         server.pluginManager.registerEvents(KokoroIoEventListener(), this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
-        webserver.stop(gracePeriod = 100, timeout = 100, timeUnit = TimeUnit.SECONDS)
+        //notifier?.postMessage("Server stopping.")
+        webserver.stop(gracePeriod = 10, timeout = 10, timeUnit = TimeUnit.SECONDS)
     }
 
     val jarFile: File
